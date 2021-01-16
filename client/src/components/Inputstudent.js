be0f1f2f35f5b7bb6716b00289212a1500a22aae
Trai Nguyen <trainguyen@trais-imac.lan>
2021-01-15 20:15:35 -0500

    adding POST from client

---
+++ client/src/components/Inputstudent.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import axios from "axios";
+import "./Inputstudent.css";
+
+class Inputstudent extends React.Component {
+    state = {
+        firstname: "",
+        lastname: "",
+        place: ""
+    }
+    handleChange = (e) => {
+        console.log(e.target.name)
+        console.log(e.target.value)
+        this.setState({[e.target.name]:e.target.value})
+    }
+
+    handleSubmit = () => {
+        if(this.state.firstname !=="" && this.state.lastname !=="" && this.state.place !=="" ) {
+            axios
+                .post("/students", this.state)
+                .then(res => {
+                    console.log("sucessfully posted on DB");
+                })
+                .catch (err => console.log(err.msg))
+        }
+    }
+    render() {
+        return (
+            <div className="row text-center">
+                <div className="col-md-4">
+                    <form onSubmit={() => this.handleSubmit()}>
+                        <input 
+                            required
+                            onChange={(e) => this.handleChange(e)} 
+                            name="firstname" value={this.state.firstname} 
+                            type="text" className="form-control form-input" 
+                            placeholder="First Name"
+                        />
+                        <input 
+                            required
+                            onChange={(e) => this.handleChange(e)} 
+                            name="lastname" value={this.state.lastname} 
+                            type="text" className="form-control form-input" 
+                            placeholder="Last Name"/>
+                        <input 
+                            onChange={(e) => this.handleChange(e)} 
+                            name="place" value={this.state.place} type="text" 
+                            className="form-control form-input" 
+                            placeholder="Place"/>
+                        <button type="submit" className="form-button btn">CREATE</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+export default Inputstudent;
